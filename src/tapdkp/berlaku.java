b73c6274c381283fc02854d1427ce246b7248b7e
nabilahanas <mizunaraya@gmail.com>
2021-06-01 09:17:35 +0800

    commit

---
+++ src/tapdkp/berlaku.java
@@ -0,0 +1,13 @@
+package tapdkp;
+
+public class berlaku {
+    private String berlaku;
+
+    public void setBerlaku(String berlaku){
+        this.berlaku=berlaku;
+
+}
+    public String getberlaku(){
+        return berlaku;
+    }
+}
